7925d789dc10392ec98998e40f15da09f705dc8c
Erik1007 <erik.hartman1007@gmail.com>
2022-12-02 09:00:01 +0000

    added html templates for each page of the site, added background media video, added context for the main restuarant and bar menus, added the first view for reviews and added style.css framework

--- food/views.py
+++ food/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
+
+
+class PostList(generic.ListView):
+    model = Post
+    queryset = Post.objects.order_by('-created_on')
+    template_name = 'reviews.html'
+    paginate_by = 12
 
 # Create your views here.
