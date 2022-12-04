f3505ce14857bf2ff45e30c776307f8054d9ebd6
Erik1007 <erik.hartman1007@gmail.com>
2022-12-04 06:36:49 +0000

    added reseravtions app and start of reservation models for the booking system

--- alice/urls.py
+++ alice/urls.py
@@ -18,7 +18,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("blog.urls"), name="blog-urls"),
+    path("", include("food.urls"), name="food-urls"),
     path('summernote/', include('django_summernote.urls')),
     path("accounts/", include("allauth.urls")),
+    
 ]
--- reservation/models.py
+++ reservation/models.py
@@ -2,4 +2,16 @@ from django.db import models
 
 
 class Reservation(models.Model):
-    
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=150)
+    phone = models.IntegerField()
+    number_of_persons = models.IntegerField()
+    date = models.DateField()
+    time = models.TimeField()
+
+
+class table():
+
+
+class Customer():
+
