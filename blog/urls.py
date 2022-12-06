b03796771c9dea824e7252a9e184af29d804cb6b
Erik1007 <erik.hartman1007@gmail.com>
2022-12-06 11:02:54 +0000

    changed the urls in the app by removing the html tag from the path in an attempt to get the project to run

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('reviews.html', views.PostList.as_view(), name='review'),
+    path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
-
 ]
