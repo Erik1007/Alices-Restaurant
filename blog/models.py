f175011c16d954f9bed1b3b5255c121161c7efd8
Erik1007 <erik.hartman1007@gmail.com>
2022-12-08 21:03:01 +0000

    working on blog so that the uers comments can be rendered and so that we admin can approve comments

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
+
 STATUS = ((0, "Draft"), (1, "Published"))
 
 
@@ -9,23 +10,20 @@ class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="review_posts"
+        User, on_delete=models.CASCADE, related_name="blog_posts"
     )
-    created_on = models.DateTimeField(auto_now_add=True)
-    status = models.IntegerField(choices=STATUS, default=0)
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
-#    likes = models.ManyToManyField(
-#        User, related_name='blogpost_like', blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
 
     class Meta:
         ordering = ["-created_on"]
 
     def __str__(self):
         return self.title
-
-#    def number_of_likes(self):
-#        return self.likes.count()
 
 
 class Comment(models.Model):
--- blog/views.py
+++ blog/views.py
@@ -21,9 +21,6 @@ class PostDetail(View):
     def get(self, request, slug, *args, **kwargs):
         post = get_object_or_404(queryset, slug=slug)
         comments = post.comments.filter(approved=True).order_by("-created_on")
-#        liked = False
-#        if post.likes.filter(id=self.request.user.id).exists():
-#            liked = True
 
         return render(
             request,
@@ -31,18 +28,13 @@ class PostDetail(View):
             {
                 "post": post,
                 "comments": comments,
-#               "liked": liked,
                 "comment_form": CommentForm()
             },
         )
 
     def post(self, request, slug, *args, **kwargs):
-#        queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
         comments = post.comments.filter(approved=True).order_by("-created_on")
-#        liked = False
-#        if post.likes.filter(id=self.request.user.id).exists():
-#            liked = True
 
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
@@ -62,18 +54,6 @@ class PostDetail(View):
                 "comments": comments,
                 "commented": True,
                 "comment_form": comment_form,
-#               "liked": liked
             },
         )
 
-
-# class PostLike(View):
-  
-#    def post(self, request, slug, *args, **kwargs):
-#        post = get_object_or_404(Post, slug=slug)
-#        if post.likes.filter(id=request.user.id).exists():
-#            post.likes.remove(request.user)
-#        else:
-#            post.likes.add(request.user)
-
-#        return HttpResponseRedirect(reverse('reviews', args=[slug]))
