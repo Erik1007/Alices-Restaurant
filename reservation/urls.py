78a5455ee06360cb785d6b88517adde65d279449
Erik1007 <erik.hartman1007@gmail.com>
2023-05-04 05:54:00 +0000

    added delete reservation code, created success html with info, changed reservation submit button to link to success page, removed popup modal, changed url connection for new reservation pages, deguggin url links

--- reservation/urls.py
+++ reservation/urls.py
@@ -6,9 +6,6 @@ from .views import search_reservation, delete_reservation, update_reservation
 app_name = "reservation"
 
 urlpatterns = [
-    path('reservation_details/', views.reservation_details, name='reservation_details'),
-    path('search_reservation/', views.search_reservation, name='search_reservation'),
-    path('update_reservation/', views.update_reservation, name='update_reservation'),
-    path('delete_reservation/', views.delete_reservation, name='delete_reservation'),
+    
     path('', views.reserve_table, name='reserve_table'),
     ]
