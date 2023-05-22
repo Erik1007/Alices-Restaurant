1e0116cdecf87ab5640ab7a56f2c98085352374f
Erik1007 <erik.hartman1007@gmail.com>
2023-05-22 11:11:18 +0000

    migrated the blog.models

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from . import views
 from django.urls import path
+from .views import PostDetail
 
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('reviews/<slug:slug>/', PostDetail.as_view(), name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,7 @@ from .forms import CommentForm
 class HomeScreen(View):
     def get(self, request, *args, **kwargs):
         return render(request, "index.html")
-
+        
 
 class PostList(generic.ListView):
     model = Post
@@ -17,23 +17,23 @@ class PostList(generic.ListView):
 
 
 class PostDetail(View):
-
     def get(self, request, slug, *args, **kwargs):
-        post = get_object_or_404(queryset, slug=slug)
+        post = get_object_or_404(Post, slug=slug)
         comments = post.comments.filter(approved=True).order_by("-created_on")
-
+        comment_form = CommentForm()
+        
         return render(
             request,
             "reviews.html",
             {
                 "post": post,
                 "comments": comments,
-                "comment_form": CommentForm()
+                "comment_form": comment_form
             },
         )
 
     def post(self, request, slug, *args, **kwargs):
-        post = get_object_or_404(queryset, slug=slug)
+        post = get_object_or_404(Post, slug=slug)
         comments = post.comments.filter(approved=True).order_by("-created_on")
 
         comment_form = CommentForm(data=request.POST)
@@ -43,8 +43,6 @@ class PostDetail(View):
             comment = comment_form.save(commit=False)
             comment.post = post
             comment.save()
-        else:
-            comment_form = CommentForm()
 
         return render(
             request,
